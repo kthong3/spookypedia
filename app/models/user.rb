5641668e5595863318b311986f4fc817f1b2e377
Brian Lin <lin.brian.l@gmail.com>
2017-10-11 15:23:35 -0500

    Add secure password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :articles, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :active_categories, through: :articles, source: :category
+
+  has_secure_password
 end
