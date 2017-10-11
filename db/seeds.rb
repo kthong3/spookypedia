98d57e1545eb2fa838bb182107fa26d2dfe89b8e
Sam Tietze <samtietze@gmail.com>
2017-10-11 15:11:09 -0500

    Add unique user conditional

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,16 @@ admin = [1,1,1,2,2,2]
   admin_user.save
 end
 
-10.times do
+20.times do
   spooky_user = User.new(username: Faker::GameOfThrones.character, password: 'password', admin: false)
   spooky_user.email = Faker::Internet.safe_email(spooky_user.username)
   spooky_user.profile_pic_url = Faker::Avatar.image(spooky_user.username, "300x300", "png", "set4", "bg1")
-  spooky_user.save
+  if !spooky_user.save
+    spooky_user.username = Faker::Hobbit.character
+    spooky_user.email = Faker::Internet.safe_email(spooky_user.username)
+    spooky_user.profile_pic_url = Faker::Avatar.image(spooky_user.username, "300x300", "png", "set4", "bg1")
+    spooky_user.save
+  end
 end
 
 Category.create!(name: 'How-To')
