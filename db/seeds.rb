98ce28c07e9dc23f4ed18ca17ed40e436d2795fa
Brian Lin <lin.brian.l@gmail.com>
2017-10-11 15:19:12 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@ require 'faker'
 
 admin = [1,1,1,2,2,2]
 2.times do
-  admin_user = User.new(username: "Admin #{admin.shift}", password: "password", email: "admin#{admin.shift}@admin.none", profile_pic_url = Faker::Avatar.image("#{admin.shift}", "300x300", "png", "set4", "bg1"))
+  admin_user = User.new(username: "Admin #{admin.shift}", password: "password", email: "admin#{admin.shift}@admin.none", is_admin: true, profile_pic_url = Faker::Avatar.image("#{admin.shift}", "300x300", "png", "set4", "bg1"))
   admin_user.save
 end
 
 20.times do
-  spooky_user = User.new(username: Faker::GameOfThrones.character, password: 'password', admin: false)
+  spooky_user = User.new(username: Faker::GameOfThrones.character, password: 'password', is_admin: false)
   spooky_user.email = Faker::Internet.safe_email(spooky_user.username)
   spooky_user.profile_pic_url = Faker::Avatar.image(spooky_user.username, "300x300", "png", "set4", "bg1")
   if !spooky_user.save
