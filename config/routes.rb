3b7f6bfb82e5ab45b6007f5efc13770d01da7f99
Brian Lin <lin.brian.l@gmail.com>
2017-10-11 15:02:49 -0500

    Create routes and controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, except: [:destroy] do
+    resources :admins, only: [:index, :new, :create, :destroy]
+  end
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :categories
+  resources :articles do
+    resources :comments, except: [:index]
+  end
+
 end
