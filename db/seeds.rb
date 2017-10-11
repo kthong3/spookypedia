434e7eff857876e9e1f1163972c21b691df45c5e
Sam Tietze <samtietze@gmail.com>
2017-10-11 14:42:43 -0500

    Write user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+admin = [1,1,1,2,2,2]
+2.times do
+  admin_user = User.new(username: "Admin #{admin.shift}", password: "password", email: "admin#{admin.shift}@admin.none", profile_pic_url = Faker::Avatar.image("#{admin.shift}", "300x300", "png", "set4", "bg1"))
+  admin_user.save
+end
+
+10.times do
+  spooky_user = User.new(username: Faker::GameOfThrones.character, password: 'password', admin: false)
+  spooky_user.email = Faker::Internet.safe_email(spooky_user.username)
+  spooky_user.profile_pic_url = Faker::Avatar.image(spooky_user.username, "300x300", "png", "set4", "bg1")
+  spooky_user.save
+end
+
+
