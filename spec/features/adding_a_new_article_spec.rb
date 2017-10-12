badc74530882c227523d00d36a5ff7f26dbbe948
byzantinist <echiang777@yahoo.com>
2017-10-12 12:05:08 -0500

    Write passing spec test for drafting a new article

--- spec/features/adding_a_new_article_spec.rb
+++ spec/features/adding_a_new_article_spec.rb
@@ -20,7 +20,30 @@ feature 'add article page' do
     expect(page).to have_current_path articles_path
     expect(page.all('li')[0].text).to eq "Title can't be blank"
     expect(page.all('li')[1].text).to eq "Body can't be blank"
-
-
   end
+
+  scenario "the user drafts a new article from the user profile page" do
+    user = User.create(username: "redpanda", email: "red@panda.com", password: "redpanda", password_confirmation: "redpanda")
+    category = Category.create(name: "critters")
+
+    visit '/sessions'
+
+    fill_in('user[email]', :with => "#{user.email}")
+    fill_in('user[password]', :with => "#{user.password}")
+    find("input[type='submit']").click
+
+    visit "/users/#{user.id}"
+
+    find("a", :text => "Create a New Article!").click
+
+    fill_in('article[title]', :with => "Capybaras")
+    fill_in('article[body]', :with => "Are the largest rodents!")
+
+    find("input[type='submit']").click
+
+    expect(page).to have_current_path user_path(user)
+  end
+
+
+
 end
