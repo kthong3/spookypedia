35d3161010497b8acd400e5ff294e2976fd85b7c
Brian Lin <lin.brian.l@gmail.com>
2017-10-11 21:03:53 -0500

    Create login and logout feature.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   resources :users, except: [:destroy] do
     resources :admins, only: [:index, :new, :create, :destroy]
   end
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create]
   resources :categories
   resources :articles do
     resources :comments, except: [:index]
   end
 
   root 'categories#index'
+
+  delete "/sessions" => 'sessions#destroy', as: :logout
 end
