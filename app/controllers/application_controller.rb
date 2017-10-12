fd5af11c21f5b4650d4e76c0eafabb807031a7af
Brian Lin <lin.brian.l@gmail.com>
2017-10-11 20:18:09 -0500

    Add helper methods for authentication in application_controller and changed layout for logging in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,36 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :logged_in?
+  helper_method :authorized?
+  helper_method :current_user
+
+  def find_and_ensure_article(id)
+    article = Article.find_by(id: id)
+    render :file => "#{Rails.root}/public/404.html", :status => 404 unless article
+    entry
+  end
+
+  def logged_in?
+    !session[:user_id].nil?
+  end
+
+  def authenticate!
+    if !logged_in?
+      @redirect = true
+      render "/sessions/new"
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def authorized?(owner)
+    current_user == owner
+  end
+
+  def authorize!(owner)
+    render :file => "#{Rails.root}/public/404.html", :status => 404 unless authorized?(owner)
+  end
 end
