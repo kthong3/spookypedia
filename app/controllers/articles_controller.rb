f25796733f68751605fcab660396c2511de11130
byzantinist <echiang777@yahoo.com>
2017-10-12 09:16:41 -0500

    Basic add article viewpage is ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,17 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @categories = Category.all
+    p "Categories array is: #{@categories}"
+    p "******************"
   end
 
   def create
+    p "******************"
+    p "Category is: #{post_params[:test_category]}"
+    p "Title is: #{post_params[:title]}"
+    p "Body is: #{post_params[:body]}"
+
   end
 
   def show
@@ -26,6 +34,10 @@ class ArticlesController < ApplicationController
     article = Article.find_by(id: id)
     render :file => "#{Rails.root}/public/404.html", :status => 404 unless article && article.is_published == true
     article
+  end
+
+  def post_params
+    params.require(:article).permit(:new_article_category, :test_category, :title, :body)
   end
 
 end
