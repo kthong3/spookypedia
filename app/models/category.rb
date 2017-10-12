f205ec5be0879f04fcf9db5ce2fd03b0ea5363a1
Sam Tietze <samtietze@gmail.com>
2017-10-12 15:28:07 -0500

    Change category header

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,10 @@ class Category < ApplicationRecord
   has_many :contributors, through: :articles, source: :author
   has_many :comments, through: :articles
 
+  def top_articles
+    self.articles.where
+    articles.comments.count
+  end
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
