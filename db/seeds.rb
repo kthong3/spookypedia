794274f5a505e82f0c758a243b4cfb58f66734ef
Sam Tietze <samtietze@gmail.com>
2017-10-13 15:54:11 -0500

    Write new body for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,11 @@ Category.delete_all
 Article.delete_all
 Comment.delete_all
 
-admin = [1,1,1,2,2,2]
-2.times do
-  admin_user = User.new(username: "Admin #{admin.shift}", password: "password", email: "admin#{admin.shift}@admin.none", is_admin: true, profile_pic_url: Faker::Avatar.image("#{admin.shift}", "300x300", "png", "set4", "bg1"))
-  admin_user.save
-end
+admin_user = User.new(username: "Spooky Admin", password: "32822c-2fa8d0", email: "spooky_avner@test.none", is_admin: true, profile_pic_url: Faker::Avatar.image("spooky-avner", "300x300", "png", "set4", "bg1"))
+admin_user.save
 
-20.times do
+
+5.times do
   spooky_user = User.new(username: Faker::GameOfThrones.character, password: 'password', is_admin: false)
   spooky_user.email = Faker::Internet.safe_email(spooky_user.username)
   spooky_user.profile_pic_url = Faker::Avatar.image(spooky_user.username, "300x300", "png", "set4", "bg1")
@@ -35,20 +33,20 @@ Category.create!(name: 'Kreepy Kids')
 
 authors = User.pluck(:id)
 categories = Category.pluck(:id)
-20.times do
-  article = Article.new(title: Faker::Lovecraft.tome, body: Faker::Lovecraft.paragraph(rand(10..20)), author_id: authors.sample, category_id: categories.sample, is_published: true)
+5.times do
+  article = Article.new(title: Faker::Lovecraft.tome, body: "Eldritch tenebrous indescribable unutterable daemoniac iridescence nameless abnormal. Antiquarian antediluvian daemoniac fungus charnel gibbous squamous accursed. Manuscript lurk decadent iridescence gibbering. Dank loathsome tenebrous. Singular unnamable shunned eldritch noisome spectral non-euclidean madness. Immemorial unmentionable foetid effulgence tenebrous stygian. Decadent non-euclidean gibbous stygian fungus unutterable. Amorphous antiquarian lurk unmentionable charnel antediluvian. Fainted swarthy mortal gibbous. Stygian shunned iridescence cyclopean gambrel dank immemorial. Madness shunned indescribable. Foetid loathsome tentacles. \n
+
+    Fainted singular immemorial tentacles blasphemous unnamable charnel. Dank fainted nameless gibbous. Spectral comprehension singular iridescence shunned dank immemorial unnamable. Stygian unnamable madness spectral tentacles noisome manuscript charnel. Ululate stench blasphemous. Iridescence singular mortal ululate blasphemous. Cyclopean squamous charnel. \n
+
+    Gibbous abnormal furtive antiquarian. Spectral cyclopean loathsome hideous immemorial ululate antediluvian stench. Non-euclidean spectral nameless noisome daemoniac stench charnel. Antiquarian unnamable gibbering indescribable blasphemous daemoniac spectral swarthy. Nameless effulgence spectral noisome.", author_id: authors.sample, category_id: categories.sample, is_published: true)
   article.save
 
-  article = Article.new(title: Faker::Lovecraft.location, body: Faker::Lovecraft.paragraph(rand(20..40)), author_id: authors.sample, category_id: categories.sample, is_published: true)
-  article.save
-
-  article = Article.new(title: Faker::StarTrek.villain, body: Faker::Lovecraft.paragraph(rand(10..20)), author_id: authors.sample, category_id: categories.sample, is_published: true)
-  article.save
-
-  article = Article.new(title: Faker::Zelda.location, body: Faker::Lovecraft.paragraph(rand(10..40)), author_id: authors.sample, category_id: categories.sample, is_published: true)
+  article = Article.new(title: Faker::Lovecraft.location, body: "Fainted mortal madness blasphemous decadent eldritch. Foetid decadent gibbering singular cat amorphous unmentionable. Dank nameless spectral foetid ululate. Lurk antiquarian indescribable accursed eldritch squamous. Foetid gibbous swarthy. Comprehension stench tentacles abnormal indescribable singular tenebrous shunned. Cat ululate daemoniac amorphous. Stygian mortal accursed effulgence. Antediluvian non-euclidean hideous nameless. Mortal non-euclidean unmentionable. Dank furtive manuscript amorphous iridescence unmentionable swarthy squamous. Gambrel gibbering nameless dank iridescence. Lurk daemoniac fainted comprehension stygian. Immemorial tentacles mortal iridescence foetid blasphemous fainted cat. Accursed gibbering swarthy unutterable effulgence spectral. Charnel squamous iridescence furtive unnamable blasphemous daemoniac amorphous. \n
+    Gibbous abnormal furtive antiquarian. Spectral cyclopean loathsome hideous immemorial ululate antediluvian stench. Non-euclidean spectral nameless noisome daemoniac stench charnel. Antiquarian unnamable gibbering indescribable blasphemous daemoniac spectral swarthy. Nameless effulgence spectral noisome. \n
+    Mortal hideous tenebrous amorphous cat stench. Madness singular cat manuscript lurk cyclopean eldritch. Stygian gibbering noisome nameless lurk eldritch. Comprehension spectral blasphemous. Lurk madness unmentionable comprehension. Charnel gambrel antediluvian. Stygian decadent swarthy. Iridescence gibbering nameless comprehension indescribable effulgence unutterable manuscript. Swarthy comprehension antiquarian unnamable non-euclidean amorphous gibbous. Gambrel shunned immemorial furtive squamous ululate.", author_id: authors.sample, category_id: categories.sample, is_published: true)
   article.save
 end
 
-20.times do
-  Comment.create!(article_id: Article.pluck(:id).sample, author_id: User.pluck(:id).sample, body: Faker::Lovecraft.tome)
-end
+# 20.times do
+#   Comment.create!(article_id: Article.pluck(:id).sample, author_id: User.pluck(:id).sample, body: Faker::Lovecraft.tome)
+# end
