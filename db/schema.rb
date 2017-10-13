ed78e860231224523974db6db91d78995dae05a3
byzantinist <echiang777@yahoo.com>
2017-10-13 10:24:04 -0500

    Add migration change to add bio field to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012134734) do
+ActiveRecord::Schema.define(version: 20171013151018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,9 +71,9 @@ ActiveRecord::Schema.define(version: 20171012134734) do
     t.string "profile_pic_url"
     t.boolean "is_admin"
     t.boolean "is_banned"
-    t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "bio"
   end
 
 end
