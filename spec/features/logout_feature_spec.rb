35428ab59bda46b8f5146cc8361da86aa1058ebb
byzantinist <echiang777@yahoo.com>
2017-10-13 16:49:16 -0500

    Fix logout_feature_spec

--- spec/features/logout_feature_spec.rb
+++ spec/features/logout_feature_spec.rb
@@ -3,16 +3,17 @@ feature 'logout link' do
 
   scenario 'the user logs in from the homepage and logs out' do
     author1 = User.create(username: "TestUser", is_admin: "false", email: "test@test.none", password: "password")
+    category = Category.create(name: "critters")
+    article = category.articles.create(title: "This is a Halloween post", body: "STuff and things and whatnot.", author_id: author1.id, is_published: true)
 
-    visit '/categories'
-
-    find("a", :text => "Login").click
-
+    visit new_session_path
     expect(page).to have_current_path new_session_path
 
-    fill_in('user[email]', :with => "test@test.none")
-    fill_in('user[password]', :with => 'password')
-    find("input[type='submit']").click
+    within(".user-views") do
+        fill_in('user[email]', :with => "test@test.none")
+        fill_in('user[password]', :with => 'password')
+        find("input[type='submit']").click
+    end
 
     expect(page).to have_current_path categories_path
     expect(page.all('a')[0].text).to eq author1.username
